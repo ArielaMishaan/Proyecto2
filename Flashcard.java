ed29872e77fe844aebcf329c44d5ffd4273afdf1
Diego Soto <sot22737@uvg.edu.gt>
2022-09-28 12:06:34 -0600

    Update Flashcard.java
    ya termine

--- Flashcard.java
+++ Flashcard.java
@@ -1,3 +1,43 @@
 public class Flashcard {
+    private String lado1;
+    private String lado2;
+
+
+    public Flashcard() {
+        lado1 = "";
+        lado2 = "";
+    }
+
+
+    public Flashcard(String lado1, String lado2) {
+        this.lado1 = lado1;
+        this.lado2 = lado2;
+    }
+
+
+    public String getLado1() {
+        return this.lado1;
+    }
+
+    public void setLado1(String lado1) {
+        this.lado1 = lado1;
+    }
+
+    public String getLado2() {
+        return this.lado2;
+    }
+
+    public void setLado2(String lado2) {
+        this.lado2 = lado2;
+    }
+
+
+    @Override
+    public String toString() {
+        return "{" +
+            " lado1='" + getLado1() + "'" +
+            ", lado2='" + getLado2() + "'" +
+            "}";
+    }
     
 }
