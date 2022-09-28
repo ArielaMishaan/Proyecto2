9b45b12c7663d1be43505d095aa810d389666dee
Diego Soto <sot22737@uvg.edu.gt>
2022-09-28 11:36:50 -0600

    Update Libro.java
    ezpz

--- Libro.java
+++ Libro.java
@@ -3,4 +3,65 @@ public class Libro{
     private String tema;
     private int paginas;
     private String idioma;
+
+
+    public Libro() {
+        nombre = "";
+        tema = "";
+        paginas = 0;
+        idioma = "";
+    }
+
+
+    public Libro(String nombre, String tema, int paginas, String idioma) {
+        this.nombre = nombre;
+        this.tema = tema;
+        this.paginas = paginas;
+        this.idioma = idioma;
+    }
+
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTema() {
+        return this.tema;
+    }
+
+    public void setTema(String tema) {
+        this.tema = tema;
+    }
+
+    public int getPaginas() {
+        return this.paginas;
+    }
+
+    public void setPaginas(int paginas) {
+        this.paginas = paginas;
+    }
+
+    public String getIdioma() {
+        return this.idioma;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " nombre='" + getNombre() + "'" +
+            ", tema='" + getTema() + "'" +
+            ", paginas='" + getPaginas() + "'" +
+            ", idioma='" + getIdioma() + "'" +
+            "}";
+    }
+
+
 }
