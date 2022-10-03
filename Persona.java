5ead4f74fb2ebfae2269eccaa863f5b73bc72870
ignaciomendeza <109829839+ignaciomendeza@users.noreply.github.com>
2022-10-03 09:35:26 -0600

    Persona (update)
    Definición de atributos.

--- Persona.java
+++ Persona.java
@@ -1,3 +1,11 @@
+import java.util.ArrayList;
+
 public class Persona{
+    private String nombre;
+    private ArrayList<Libro> listaLibros;
+    private ArrayList<ListaFlashcards> listaListaFlashcards;
+    private String metas;
+    private String carnet;
+    private String contrasenia;
     
 }
