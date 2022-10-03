2f9b6cf3730538780f8e89aa74b737b438d6e483
ignaciomendeza <109829839+ignaciomendeza@users.noreply.github.com>
2022-10-03 09:35:01 -0600

    ListaFlashcards (update)
    Se definieron los atributos y métodos de la clase ListaFlashcards.

--- ListaFlashcards.java
+++ ListaFlashcards.java
@@ -1,3 +1,51 @@
+import java.util.ArrayList;
+
 public class ListaFlashcards{
+    private String tema;
+    private ArrayList<Flashcard> listaFlashcards;
+
+    public ListaFlashcards() {
+        tema = "";
+        listaFlashcards = new ArrayList<Flashcard>();
+    }
+
+    public ListaFlashcards(String tema, ArrayList<Flashcard> listaFlashcards) {
+        this.tema = tema;
+        this.listaFlashcards = listaFlashcards;
+    }
+
+    public String getTema() {
+        return this.tema;
+    }
+
+    public void setTema(String tema) {
+        this.tema = tema;
+    }
+
+    public ArrayList<Flashcard> getListaFlashcards() {
+        return this.listaFlashcards;
+    }
+
+    public void setListaFlashcards(ArrayList<Flashcard> listaFlashcards) {
+        this.listaFlashcards = listaFlashcards;
+    }
+
+    public void agregarFlashcard(String lado1, String lado2){
+        Flashcard nueva = new Flashcard(lado1, lado2);
+        listaFlashcards.add(nueva);
+    }
+
+    @Override
+    public String toString() {
+        String resultado = "";
+        for (Flashcard flashcard : listaFlashcards) {
+            resultado = resultado + flashcard.toString();
+        }
+
+        return resultado;
+    }
+
+
+    
 
 }
