fa44445660f111f463a2143fa36cc29f5e8236a6
ArielaMishaan <109829847+ArielaMishaan@users.noreply.github.com>
2022-10-16 19:44:17 -0600

    Clase Archivo
    para leer, guardar y borrar archivos

---
+++ Archivo.java
@@ -0,0 +1,77 @@
+/**
+ * clase que recolecta los datos
+ * @authot Alina Carías, Ignacio Mendez, Diego Soto, Ariela Mishaan
+ * @date 07-10-2022
+ */
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.FileWriter;
+
+public class Archivo{
+    
+    private File archivo;
+
+    public Archivo (String nombreArchivo){
+        archivo = new File(nombreArchivo);
+        try{
+            archivo.createNewFile();   
+        } 
+        catch(IOException e){
+            //si el archivo no existe o está en uso
+            e.printStackTrace();
+        }
+    }
+
+    public void escribirArchivo(String linea){
+        try{
+            FileWriter escritor = new FileWriter(archivo);
+            escritor.write(linea);
+            escritor.close();
+
+        } 
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<String> leerArchivo(){
+        Scanner lector;
+        ArrayList<String> lineas = new ArrayList<String>();
+
+        try{
+            lector = new Scanner(archivo);
+            while(lector.hasNextLine()){
+                lineas.add(lector.nextLine());
+            }
+        }
+        catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+
+        return lineas;
+    }
+
+    public String leerArchivoString(){
+        String texto = "";
+
+        try{
+            Scanner lector = new Scanner(archivo);
+            while(lector.hasNextLine()){
+                texto = texto + lector.nextLine() + "\n";
+            }
+        }
+        catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+
+        return texto;
+    }
+
+    public void eliminarArchivo(){
+        archivo.delete();
+    }
+}
--- Datos.java
+++
@@ -1,8 +0,0 @@
-/**
- * clase que recolecta los datos
- * @authot Alina Carías, Ignacio Mendez, Diego Soto, Ariela Mishaan
- * @date 07-10-2022
- */
-public class Datos{
-    
-}
